d91878a26d8e8aace811b95295aaa164239fc0b0
김민기 <rlaalsrl9569@naver.com>
2021-03-27 21:13:36 +0900

    keyborad event

--- game.py
+++ game.py
@@ -21,6 +21,11 @@ character_height = character_size[1]
 character_x_pos = ((screen_width / 2) - (character_width / 2))  # 화면 가로의 절반에 위치
 character_y_pos = screen_height - character_height # 화면 세로의 가장 밑에 위치
 
+
+# 이동 할 좌표
+to_x = 0
+to_y = 0
+
 # 이벤트 루프
 running = True
 
@@ -28,6 +33,32 @@ while(running):
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # close button
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                to_x -= 5
+            elif event.key == pygame.K_RIGHT:
+                to_x += 5
+            elif event.key == pygame.K_UP:
+                to_y -= 5
+            elif event.key == pygame.K_DOWN:
+                to_y += 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0
+
+    character_x_pos += to_x
+    character_y_pos += to_y
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+    if character_y_pos < 0:
+        character_y_pos = 0
+    elif character_y_pos > screen_height - character_height:
+        character_y_pos = screen_height - character_height
 
     screen.blit(background, (0, 0))
     screen.blit(character, (character_x_pos, character_y_pos))
