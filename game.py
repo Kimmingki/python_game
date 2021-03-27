215926a297d6020ca2ed2ee4891732b8a6d5c523
김민기 <rlaalsrl9569@naver.com>
2021-03-27 21:00:10 +0900

    make character

--- game.py
+++ game.py
@@ -13,6 +13,14 @@ pygame.display.set_caption("Nado Game")
 # 배경 이미지 불러오기
 background = pygame.image.load("/Users/kimminki/Documents/python_game/background.png")
 
+# 캐릭터 (스프라이트) 불러오기
+character = pygame.image.load("/Users/kimminki/Documents/python_game/character.png")
+character_size = character.get_rect().size  # 이미지의 크기를 구해옴
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = ((screen_width / 2) - (character_width / 2))  # 화면 가로의 절반에 위치
+character_y_pos = screen_height - character_height # 화면 세로의 가장 밑에 위치
+
 # 이벤트 루프
 running = True
 
@@ -22,6 +30,7 @@ while(running):
             running = False
 
     screen.blit(background, (0, 0))
+    screen.blit(character, (character_x_pos, character_y_pos))
     pygame.display.update() # 화면 다시 그리기
 
 # 게임 종료
