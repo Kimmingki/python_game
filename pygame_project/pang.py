cbd5109c674daf2aac56886f2853dff74470b52d
김민기 <rlaalsrl9569@naver.com>
2021-03-31 14:07:22 +0900

    Delete weapon and ball

--- pygame_project/pang.py
+++ pygame_project/pang.py
@@ -60,7 +60,7 @@ ball_images = [
 ]
 
 # 공의 크기에 따른 최초 속도
-ball_speed_y = [-18, -15, -12, -9]
+ball_speed_y = [-13, -10, -7, -4]
 
 # 공들
 balls = []
@@ -74,6 +74,9 @@ balls.append({
     "to_y": -6,
     "init_spd_y": ball_speed_y[0]
 })
+
+weapon_to_remove = -1
+ball_to_remove = -1
 
 running = True
 
@@ -129,12 +132,50 @@ while(running):
         if ball_pos_y >= screen_height - stage_height - ball_height:
             ball_val["to_y"] = ball_val["init_spd_y"]
         else:
-            ball_val["to_y"] += 0.5
+            ball_val["to_y"] += 0.3
 
         ball_val["pos_x"] += ball_val["to_x"]
-        ball_val["pos_y"] += ball_val["to_y"]        
-    # 4. 충돌 처리
+        ball_val["pos_y"] += ball_val["to_y"]
 
+
+    # 4. 충돌 처리
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    for ball_idx, ball_val in enumerate(balls):
+        ball_pos_x = ball_val["pos_x"]
+        ball_pos_y = ball_val["pos_y"]
+        ball_img_idx = ball_val["img_idx"]
+
+        ball_rect = ball_images[ball_img_idx].get_rect()
+        ball_rect.left = ball_pos_x
+        ball_rect.top = ball_pos_y
+
+        if character_rect.colliderect(ball_rect):
+            running = False
+            break
+        
+        for weapon_idx, weapon_val in enumerate(weapons):
+            weapon_pos_x = weapon_val[0]
+            weapon_pos_y = weapon_val[1]
+            
+            weapon_rect = weapon.get_rect()
+            weapon_rect.left = weapon_pos_x
+            weapon_rect.top = weapon_pos_y
+
+            if weapon_rect.colliderect(ball_rect):
+                weapon_to_remove = weapon_idx
+                ball_to_remove = ball_idx
+                break
+    
+    if ball_to_remove > -1:
+        del balls[ball_to_remove]
+        ball_to_remove = -1
+        
+    if weapon_to_remove > -1:
+        del weapons[weapon_to_remove]
+        weapon_to_remove = -1
 
     # 5. 화면에 그리기
     screen.blit(background, (0, 0))
